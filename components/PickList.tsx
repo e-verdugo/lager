f98d5b73042bd9906bb2cb6d160fc6fb1678edf7
Emelie Verdugo <emelieverdugo@protonmail.com>
2022-04-12 23:34:06 +0100

    Fixed the too few message appearing once per product

--- components/PickList.tsx
+++ components/PickList.tsx
@@ -5,6 +5,8 @@ import productModel from '../models/products';
 
 export default function PickList({ route, navigation, setProducts }) {
     const { order } = route.params;
+    let canPick;
+    let isEnough = true;
 
     async function pick() {
         await orderModel.pickOrder(order);
@@ -19,13 +21,22 @@ export default function PickList({ route, navigation, setProducts }) {
             </Text>;
     });
 
-    const checkIfEnough = order.order_items.map((item, index) => {
-        if (item.amount < item.stock) {
-            return <Button title={item.name + " finns på lager. Plocka order?"} onPress={pick} key={index}/>
-        } else {
-            return <Text style={Typography.normal} key={index}>För få {item.name} i lager.</Text>
+    const checkIfEnough = order.order_items.map((item) => {
+        if (item.amount > item.stock) {
+            isEnough = false;
         }
     });
+
+    const enough = order.order_items.map((item, index) => {
+        return <Button title={item.name + " finns på lager. Plocka order?"} onPress={pick} key={index}/>
+    });
+    const notEnough = (<Text style={Typography.normal}>För få artiklar på lager.</Text>)
+
+    if (isEnough) {
+        canPick = enough;
+    } else {
+        canPick = notEnough;
+    }
 
     return (
         <ScrollView>
@@ -35,7 +46,7 @@ export default function PickList({ route, navigation, setProducts }) {
             <Text style={Typography.normal}>{order.zip} {order.city}</Text>
             <Text style={Typography.header4}>Produkter:</Text>
             {orderItemsList}
-            {checkIfEnough}
+            {canPick}
         </View>
         </ScrollView>
     )
